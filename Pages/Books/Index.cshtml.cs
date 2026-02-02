47751247211d0ad6d898ba4a1ed15f75a6ba8128
ACT-STUDENT <ACT-STUDENT@LAB3-PC23>
2026-02-02 13:17:22 +0800

    Search Bar added

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPageBooks.Data;
 using RazorPageBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RazorPageBooks.Pages.Books
 {
@@ -21,9 +22,26 @@ namespace RazorPageBooks.Pages.Books
 
         public IList<Book> Book { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Title { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? BookTitle { get; set; }
+
         public async Task OnGetAsync()
         {
-            Book = await _context.Book.ToListAsync();
+            var books = from b in _context.Book
+                        select b;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => (s.Title != null && s.Title.Contains(SearchString))
+                          || (s.Author != null && s.Author.Contains(SearchString)));
+            }
+
+            Book = await books.ToListAsync();
         }
     }
 }
