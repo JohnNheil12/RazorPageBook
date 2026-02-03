3a961b2d778f38a552fef58b3df2045e4e8bb90f
LIBRARY <LIBRARY@DESKTOP-4ISKKVS>
2026-02-03 11:47:48 +0800

    add new field

---
+++ Migrations/20260203033653_AddPublisherToBook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPageBooks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublisherToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "Book");
+        }
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -7,6 +7,6 @@
         public string? Author { get; set; }
 
         public int? YearPublished { get; set; }
-
+        public string? Publisher { get; set; }
     }
 }
