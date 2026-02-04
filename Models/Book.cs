6d50c9f064e0ac4e6ea5c804e1274f57114b21d2
LIBRARY <LIBRARY@DESKTOP-4ISKKVS>
2026-02-04 09:27:47 +0800

    adding validation

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,12 +1,24 @@
-﻿namespace RazorPageBooks.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPageBooks.Models
 {
     public class Book
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Title must have a minimum letters of 4.")]
         public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Author Name is required")]
+        [RegularExpression(@"^[a-zA-Z\s\.]+$", ErrorMessage = "Author name should only contain letters")]
         public string? Author { get; set; }
 
+        [Display(Name = "Year Published")]
+        [Range(1450, 2026, ErrorMessage = "Please enter a valid year between 1450 and 2026")]
         public int? YearPublished { get; set; }
+
+        [StringLength(50)]
         public string? Publisher { get; set; }
     }
 }
