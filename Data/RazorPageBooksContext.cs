225499825a7069bb4896b904e6077d1ae10db4cf
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2026-02-09 13:56:02 +0800

    add login, logout and register

--- Data/RazorPageBooksContext.cs
+++ Data/RazorPageBooksContext.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RazorPageBooks.Models;
 
 namespace RazorPageBooks.Data
 {
-    public class RazorPageBooksContext : DbContext
+    public class RazorPageBooksContext : IdentityDbContext
     {
         public RazorPageBooksContext (DbContextOptions<RazorPageBooksContext> options)
             : base(options)
--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RazorPageBooks.Data;
 using RazorPageBooks.Models;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<RazorPageBooksContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPageBooksContext") ?? throw new InvalidOperationException("Connection string 'RazorPageBooksContext' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<RazorPageBooksContext>();
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
